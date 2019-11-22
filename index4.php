21488cf6c174148248dc216c29d4740382dee3a9
CyrusMusic101 <31782334+CyrusMusic101@users.noreply.github.com>
2019-11-21 19:14:30 -0800

    Add files via upload

---
+++ index4.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+ <html lang="us">
+  <html>
+   <head>
+   	 <title>Welcome | Landing Page!</title>
+   	  <link rel="icon" href="https://img.icons8.com/office/40/000000/virus-free.png">
+   	  <meta charset="utf-8">
+   	  <meta name="viewport" consent="width=device-width, initial-scale=1, user-scable=no">
+        <meta http-equiv="X=UA-Compatible" content="IE=edge">
+   	  <!-- Access code for SARIN-->
+   	  <meta name="Keywords" content="V-F32K0R32KD320JF032HFJASD324J3DKKHDK3RKNR32IF32NKJ3FKJNF32UIFN23JNFMDSKF3RF84FN842NF42JKFN4U-KEYCLEARANCE-341KD31KMD31VFDVMK42VK42LVXCKLVW2424KL42542LKFM42F24">
+   	  <meta name="description" content="POSTIVE RESULT (ACOMPLISH:C1L)">
+   	  <!-- Link to resource sheets -->
+   	  <link rel="stylesheet" href="css/navbar/navbar.css"/>
+        <link rel="stylesheet" href="css/index2/slideshow.css"/>
+   	  <!-- Should SARIN link in Google Fonts? (Not Secure) -->
+   	  <link href="https://fonts.googleapis.com/css?family=Mr+De+Haviland|Neucha|Neuton" rel="stylesheet">
+   	  <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Anton|Bebas+Neue|Big+Shoulders+Text|Bowlby+One+SC|Caveat|Dancing+Script|Hind|Indie+Flower|Lato|Lobster|Montserrat|Open+Sans+Condensed:300|Oswald|Pacifico|Permanent+Marker|Playfair+Display|Raleway|Shadows+Into+Light|Staatliches|Teko|Trade+Winds&display=swap" rel="stylesheet">
+   	  <!-- Is SARIN using external APIS? If so hook below? -->
+   	  <script language="JavaScript" src="https://ssl.geoplugin.net/javascript.gp?k=8b890b3eb8bfe361" type="text/javascript"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+   </head>
+    <!-- Navigation Bar-->
+     <body>
+       <nav>
+         <label class="logo">FreePhotos.io</label>
+         <ul>
+            <li><a class="current" href="index1.html">Services</a></li>
+            <li><a href="index2.html">About</a></li>
+            <li><a href="index3.html">Team</a></li>
+            <li><a href="index4.html">Contact</a></li>
+         </ul>
+         <label id="icon"><i class="fas fa-bars"></i></label>
+      </nav>
+       <!-- Mobile Scrolling -->
+        <section></section>
+          <script type="text/javascript">
+             $(document).ready(function(){
+               $('#icon').click(function(){
+                  $('ul').toggleClass('active');
+               });
+             });
+          </script>
+           <!-- Addresses navbar rulling over content bug -->
+            <div class="godown-50" id="godown"></div>
+           <!-- Fixes godowns problems -->
+          <script>
+            $("#navMenu").resize(function () {
+               $('#godown').height($("#navMenu").height() + 10);
+               });
+                  if ($("#navMenu").height() > $('#godown').height()) $('#godown').height($("#navMenu").height() + 10);
+          </script>
+
+          <form action="" method="post">
+            <p>
+              <label for ="name">Name:</label><br>
+              <input type="text" name="name" id="name">
+            </p>
+            <p>
+              <label for ="email">Email:</label><br>
+              <input type="text" name="email" id="email">
+            </p>
+            <p>
+              <label for ="message">Message:</label><br>
+              <textarea name="message" id="message"></textarea>
+            </p>
+            <p>
+              <input type="sumbit" value="Sumbit"
